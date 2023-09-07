feb07b75e970162abffb5c70a14f034b1f6bdda8
Shridhar Vashishtha <82993782+Shridhar0305@users.noreply.github.com>
2023-09-06 20:18:56 -0500

    Preprocessing and Patch extraction applied

--- Preprocessing.py
+++ Preprocessing.py
@@ -6,6 +6,7 @@
 import SimpleITK as sitk
 import numpy as np
 import os
+from skimage.util import view_as_windows
 
 
 def apply_n4_correction(input_path, output_path):
@@ -14,9 +15,17 @@ def apply_n4_correction(input_path, output_path):
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
     corrected_image = corrector.Execute(input_image_float)
     sitk.WriteImage(corrected_image, output_path)
+
+def extract_patches(image, patch_size, stride):
+    image_array = sitk.GetArrayFromImage(image)  # Convert SimpleITK image to NumPy array
+    patches = view_as_windows(image_array, patch_size, step=stride)
+    return patches
+
     
 input_directory = "/kaggle/input"
 output_directory = "/kaggle/working"
+patch_size = (64, 64, 64)
+stride = (32, 32, 32)
 
 for root, dirs, files in os.walk(input_directory):
     for file in files:
@@ -29,5 +38,12 @@ for root, dirs, files in os.walk(input_directory):
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
             
             apply_n4_correction(input_path, output_path)
+            corrected_image = sitk.ReadImage(output_path)
+            patches = extract_patches(corrected_image, patch_size, stride)
 
-print("N4 Bias Field Correction applied to all images.")
+            for i, patch in enumerate(patches):
+                for i, patch in enumerate(patches):
+                patch_output_path = os.path.join(output_directory, f"patch_{i}.nii")
+                patch_image = sitk.GetImageFromArray(patch)
+                sitk.WriteImage(patch_image, patch_output_path)
+print("N4 Bias Field Correction and patch extraction applied to all images.")
